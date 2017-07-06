3d7c29a07b8f714b167b341e3d88293558b08919
some-PC-user <kirillmyasnikovmail@gmail.com>
2017-07-06 13:22:24 +0400

    create socket connection

--- src/views/Sintez-messages/index.js
+++ src/views/Sintez-messages/index.js
@@ -3,8 +3,51 @@ import './index.less'
 
 //import WrappedRegistrationForm from '../../views/Table/modal'
 
+const conn = new WebSocket('wss://echo.websocket.org');
+
 export default class Sintez extends React.Component {
 
+	constructor(props) {
+		super(props);
+	}
+
+	sendS (socket, message, callback) {
+		this.waitForConnection(conn, function () {
+			socket.send(message);
+			if (typeof callback !== 'undefined') {
+				callback();
+			}
+		}, 1000);
+	}
+
+	waitForConnection (socket, callback, interval) {
+		if (socket.readyState === 1) {
+			callback();
+		} else {
+			// optional: implement backoff for interval here
+			const that = this;
+			setTimeout(function () {
+				that.waitForConnection(socket, callback, interval);
+			}, interval);
+		}
+	};
+
+	componentDidMount() {
+
+		conn.onopen = function(e) {
+			console.log("Connection established!");
+		};
+		conn.onmessage = function(e) {
+			console.log(e.data);
+		};
+
+		//conn.send("Test, bla");
+
+		const session = "Test message 1";
+		this.sendS(conn, session, function () {
+			console.log('callback')
+		});
+	}
 
 	render() {
 		//const columns = this.columns;
